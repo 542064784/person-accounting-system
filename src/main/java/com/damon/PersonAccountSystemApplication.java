8bcd13301cd66987e935facc6437fece41d70ada
Damon Chen <542064784@qq.com>
2019-06-05 20:26:05 +0800

    init project

---
+++ src/main/java/com/damon/PersonAccountSystemApplication.java
@@ -0,0 +1,12 @@
+package com.damon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonAccountSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonAccountSystemApplication.class, args);
+    }
+}
